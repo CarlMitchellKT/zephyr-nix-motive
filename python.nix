4b5265a1293a01c18e3ee1749a2e989d6a21f82e
Carl Mitchell <carl.mitchell@gomotive.com>
2024-06-21 10:45:01 -0400

    Add more python libs

--- python.nix
+++ python.nix
@@ -32,5 +32,47 @@ lib.warnIf
   "zephyr-pythonEnv: Found invalid Python constraints for: ${builtins.toJSON (lib.attrNames invalidConstraints)}"
   (python.withPackages (project.renderers.withPackages {
     inherit python;
-    extraPackages = ps: [ ps.west ] ++ extraLibs;
+    extraPackages = ps: [
+      ps.west
+      ps.python-can
+      ps.pyserial
+      ps.protobuf
+      ps.dqdm
+      ps.pyelftools
+      ps.PyYAML
+      ps.pykwalify
+      ps.canopen
+      ps.packaging
+      ps.progress
+      ps.psutil
+      ps.pylink-square
+      ps.requests
+      ps.intelhex
+      ps.colorama
+      ps.ply
+      ps.coverage
+      ps.pytest
+      ps.mypy
+      ps.mock
+      ps.python-magic
+      ps.lxml
+      ps.junitparser
+      ps.pylint
+      ps.yamllint
+      ps.anytree
+      ps.gitlint
+      ps.junit2html
+      ps.clang-format
+      ps.lpc_checksum
+      ps.Pillow
+      ps.imgtool
+      ps.grpcio-tools
+      ps.PyGithub
+      ps.graphviz
+      ps.zcbor
+      ps.pyocd
+      ps.tabulate
+      ps.natsort
+      ps.cbor
+    ] ++ extraLibs;
   }))
